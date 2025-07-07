45e63e05a33bdc630e6148ff4b8be4e7da7002c8
Huy <zzcz4991@gmail.com>
2025-07-07 09:59:03 +0700

    add:khachhang-edit

--- obj/Debug/net8.0/phonev2.AssemblyInfo.cs
+++ obj/Debug/net8.0/phonev2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("phonev2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee78cf5168279bdb2c942030a81f734201a1b03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbd75750237557a76314356090d23bc7b63e8d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("phonev2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("phonev2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
